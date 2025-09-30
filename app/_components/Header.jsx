aa28a0ee6c72f475d93c5c1731f0718db9bf2ecd
sondos-saad <sondossaad812@gmail.com>
2025-09-30 20:21:04 +0300

    push

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -6,10 +6,10 @@ function Header() {
     <div className='flex items-center justify-between p-3 '>
         <h1 className='text-emerald-900 font-bold text-5xl'>AIR<span className='font-bold text-sm text-yellow-900'>Travel</span></h1>
         <div className='flex items-center gap-3 mr-6'>
-            <Link href="">Destination</Link>
-            <Link href="">Services</Link>
-            <Link href="">Booking</Link>
-            <Link href="">Testimonial</Link>
+            <Link className='font-bold text-[20px]' href="">Destination</Link>
+            <Link className='font-bold text-[20px]' href="">Services</Link>
+            <Link className='font-bold text-[20px]' href="">Booking</Link>
+            <Link className='font-bold text-[20px]' href="">Testimonial</Link>
         </div>
     </div>
   )
