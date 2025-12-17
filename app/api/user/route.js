b040e23dba89cb3f527f03fe23174881a62faff6
sondos-saad <sondossaad812@gmail.com>
2025-12-17 20:53:47 +0200

    backend

---
+++ app/api/user/route.js
@@ -0,0 +1,27 @@
+import { db } from "@/config/db"
+import { usersTable } from "@/config/schema"
+import { NextResponse } from "next/server"
+
+export async function POST(req){
+    const {email, name} = await req.json()
+
+
+
+    // if user exists
+    const user = await db.select().from(usersTable).where(eq.email, email)
+
+
+    // if user not exists
+    if(user.length == 0){
+        const result = await db.insert(usersTable).values({
+            name : name,
+            email: email
+        }).returning(usersTable)
+        console.log(result)
+
+        return NextResponse.json(result)
+    }
+
+
+     return NextResponse.json(user[0])
+}
